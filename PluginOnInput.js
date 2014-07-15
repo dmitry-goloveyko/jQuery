dc80b2388d6599c4af7c617ed645cb34db6a1c8b
dmitry-goloveyko <d.goloveyko@gmail.com>
2014-07-15 10:55:20 +0300

    Divided script and html

--- PluginOnInput.js
+++ PluginOnInput.js
@@ -1,8 +1,26 @@
-/**
- * Created by cujoPC on 14.07.2014.
- */
-( function ($) {
-    $.fn.PluginOnInput = function() {
+$(function () {
+    $("#target").click(function () {
+        var array = ['This', 'Is', 'Plugin'];
 
+        if ($("#panel").css('display') == 'none') {
+            for (var i = 0; i < array.length; i++) {
+                $("#panel").append("<div class='dropdown-value'><a>" + array[i] + "</a></div>")
+            }
+        } else {
+            $("#panel div").remove();
+        }
+
+        $("#panel").slideToggle("fast");
+        $('#panel').css("width", $("#target").width() + "px")
+
+        return false;
+    });
+});
+
+$(document).click(function (e) {
+    var container = $("#panel div");
+
+    if (!container.is(e.target)) {
+        container.remove();
     }
-})(jQuery);
+});
