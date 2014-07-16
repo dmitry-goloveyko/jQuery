41a06d6a63e93cf80eebf6fb96cdfa01bc2bf6d8
dmitry-goloveyko <d.goloveyko@gmail.com>
2014-07-16 11:41:56 +0300

    Not working

--- PluginOnInput.js
+++ PluginOnInput.js
@@ -1,26 +1,27 @@
 (function ($) {
     $.fn.dropdown = function (options) {
-        options = $.extend({
+        settings = $.extend({
             sourceArray : ['This', 'Is', 'Plugin']
         }, options);
 
         var element = $(this);
 
         var showDropdownList = function() {
-            $(this).focusin(function () {
-                var DropdownListHTML = '';
-                for(var i = 0; i < options.sourceArray.length; i++) {
-                    DropdownListHTML += '<div class="dropdown-item">' + options.sourceArray[i] + '<div>';
-                }
+            $(this)
+                .focusin (function () {
+                    var dropdownListHtml = '';
+                    for(var i = 0; i < options.sourceArray.length; i++) {
+                        dropdownListHtml += '<div class="dropdown-item">' + settings.sourceArray[i] + '<div>';
+                    }
 
-                $('#target1').after('<div class="dropdown" style="width:' + $('#target1').width() + '">' + DropdownListHTML + '</div>');
-            })
-                .focusout(function () {
+                    $(this).after('<div class="dropdown" style="width:' + $(this).width() + '">' + dropdownListHtml + '</div>');
+                })
+                .focusout (function () {
                     $('.dropdown').remove();
                 });
         };
 
-        return this.each(showDropdownList());
+        return this.each(showDropdownList);
     };
 })(jQuery);
 
